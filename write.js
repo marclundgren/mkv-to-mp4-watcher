124aad73998ec6f4f36bccb0305e6e90229ab278
Marc Lundgren <marc.lundgren@kareo.com>
2021-12-31 13:36:11 -0800

    lets go with this for now. its not removing files

--- write.js
+++ write.js
@@ -4,25 +4,34 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 const path = require("path");
 
-const mutexify = require("mutexify");
-let lock = mutexify();
+// const mutexify = require("mutexify");
+// let lock = mutexify();
 
 const write = (
   { fileName, newFileName, basePath, inExtension, outExtension = "mp4" },
   cb
 ) => {
-  console.log(`write: ${fileName}`);
-  lock(async function (release) {
-    const absolutePath = path.join(basePath, newFileName);
+  const absolutePath = path.join(basePath, newFileName);
+  console.log(`write: ${absolutePath}`);
+
+  return new Promise((resolve, reject) => {
+    // lock(async function (release) {
     console.log(`command: ${basePath}/${fileName}.${inExtension}`);
-    const command = ffmpeg(`${basePath}/${fileName}.${inExtension}`)
-      .format(outExtension)
-      .output(absolutePath)
-      .on("end", () => {
-        console.log(`done: ${absolutePath}`);
-        release(cb);
-      });
-    command.run();
+    try {
+      const command = ffmpeg(`${basePath}/${fileName}.${inExtension}`)
+        .format(outExtension)
+        .output(absolutePath)
+        .on("end", () => {
+          console.log(`done: ${absolutePath}`);
+          // release(cb);
+          resolve(absolutePath);
+        });
+      command.run();
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
+    // });
   });
 };
 module.exports = write;
