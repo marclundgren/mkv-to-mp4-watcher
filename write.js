738ca77a54669423eec9d5b09d43a9991ceb0155
Marc Lundgren <marc.lundgren@kareo.com>
2021-12-31 15:23:07 -0800

    bring back that lock

--- write.js
+++ write.js
@@ -4,8 +4,8 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 const path = require("path");
 
-// const mutexify = require("mutexify");
-// let lock = mutexify();
+const mutexify = require("mutexify");
+let lock = mutexify();
 
 const write = (
   { fileName, newFileName, basePath, inExtension, outExtension = "mp4" },
@@ -14,24 +14,25 @@ const write = (
   const absolutePath = path.join(basePath, newFileName);
   console.log(`write: ${absolutePath}`);
 
-  return new Promise((resolve, reject) => {
-    // lock(async function (release) {
-    console.log(`command: ${basePath}/${fileName}.${inExtension}`);
-    try {
-      const command = ffmpeg(`${basePath}/${fileName}.${inExtension}`)
-        .format(outExtension)
-        .output(absolutePath)
-        .on("end", () => {
-          console.log(`done: ${absolutePath}`);
-          // release(cb);
-          resolve(absolutePath);
-        });
-      command.run();
-    } catch (err) {
-      console.error(err);
-      reject(err);
-    }
-    // });
+  lock(async function (release) {
+    return new Promise((resolve, reject) => {
+      console.log(`command: ${basePath}/${fileName}.${inExtension}`);
+      try {
+        const command = ffmpeg(`${basePath}/${fileName}.${inExtension}`)
+          .format(outExtension)
+          .output(absolutePath)
+          .on("end", () => {
+            console.log(`done: ${absolutePath}`);
+            // release(cb);
+            resolve(absolutePath);
+            release(cb);
+          });
+        command.run();
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
   });
 };
 module.exports = write;
